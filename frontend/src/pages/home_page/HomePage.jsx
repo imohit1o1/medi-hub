e28f3ab05429a672039f43c712961f52b3705c76
sanjaraiy <deepakrai78788@gmail.com>
2024-07-14 13:11:37 +0530

    feat/docs/fix: give overflow-hidden class at x-axis in HomePage.jsx and give right-0 at p element where Medical Acheivement mentioned

--- frontend/src/pages/home_page/HomePage.jsx
+++ frontend/src/pages/home_page/HomePage.jsx
@@ -3,13 +3,14 @@ import { Hero, WhyUs, Testimonials } from "../../import-export/ImportExport";
 
 function Home() {
   return (
-    <>
+    //give an overflow-hidden class at x-axis
+    <div className="overflow-x-hidden">
       <Hero />
       {/* <WhyUs /> */}
       {/* <TopSpecialities /> */}
       <Testimonials />
       {/* <Contributors /> */}
-    </>
+    </div>
   );
 }
 
