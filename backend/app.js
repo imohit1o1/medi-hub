6b3589086422d9974c6ec67bbeb3254b4a038d52
Iniyal Palanisamy <123928113+IniyalPalanisamy@users.noreply.github.com>
2024-10-18 22:23:31 +0530

    Update medibackend app.js

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,4 @@
 import dotenv from "dotenv";
-
 import express from "express";
 import cookieParser from "cookie-parser";
 import { errorHandler } from "./src/utilis/ApiError.js";
@@ -12,42 +11,49 @@ dotenv.config({
   path: "./.env",
 });
 
-// cors middleware configuration connects frontend to backend
+// Validate essential environment variables
+if (!process.env.FRONTEND_URL || !process.env.DASHBOARD_URL) {
+  console.error("Essential environment variables are missing.");
+  process.exit(1); // Exit if essential variables are not set
+}
+
+// CORS middleware configuration connects frontend to backend
 app.use(
   cors({
     origin: [process.env.FRONTEND_URL, process.env.DASHBOARD_URL],
-    method: ["GET", "POST", "DELETE", "PUT"],
+    methods: ["GET", "POST", "DELETE", "PUT"],
     credentials: true,
   }),
 );
+
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// import routes
+// Import routes
 import userRouter from "./src/routes/user.routes.js";
 import contactUsRouter from "./src/routes/contactus.routes.js";
 import appointmentRouter from "./src/routes/appointment.routes.js";
 import medicineRouter from "./src/routes/medicine.routes.js";
-import CartRouter from "./src/routes/UserCart.routes.js";
-import PaymentRouter from "./src/routes/payment.routes.js";
-import TestimonialRouter from "./src/routes/testimonial.routes.js";
+import cartRouter from "./src/routes/UserCart.routes.js"; // Changed to lowercase
+import paymentRouter from "./src/routes/payment.routes.js"; // Changed to lowercase
+import testimonialRouter from "./src/routes/testimonial.routes.js"; // Changed to lowercase
 
 // Define the root route
-// app.get('/', (req, res) => {
-//   res.send('Welcome to the homepage!');
-// });
+app.get('/', (req, res) => {
+  res.send('Welcome to the homepage!');
+});
 
-// routes declaration
+// Routes declaration
 app.use("/api/v1/user", userRouter);
 app.use("/api/v1/message", contactUsRouter);
 app.use("/api/v1/appointment", appointmentRouter);
 app.use("/api/v1/medicines", medicineRouter);
-app.use("/api/v1/medicines-cart", CartRouter)
-app.use("/api/v1/payment", PaymentRouter)
-app.use("/api/v1/testimonial", TestimonialRouter)
+app.use("/api/v1/medicines-cart", cartRouter);
+app.use("/api/v1/payment", paymentRouter);
+app.use("/api/v1/testimonial", testimonialRouter);
 
-
-// error middleware
+// Error middleware
 app.use(errorHandler);
+
 export default app;
